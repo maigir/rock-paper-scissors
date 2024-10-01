8a099880fb01e61aeade3bcfdf79c715417c8ca8
Maigi Rannamets <rannamets.maigi@gmail.com>
2024-10-01 15:23:03 +0300

    Copy script.js from main branch

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
 'use strict';
+
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+  const random = ['rock', 'paper', 'scissors'];
+  const randomChoice = Math.floor(Math.random() * random.length);
+  return random[randomChoice];
+}
+
+function getHumanChoice() {
+  const human = prompt('Enter rock, paper or scissors: ').toLowerCase();
+  return human;
+}
+
+function playRound(humanChoice, computerChoice) {
+  if (
+    (humanChoice === 'rock' && computerChoice === 'scissors') ||
+    (humanChoice === 'paper' && computerChoice === 'rock') ||
+    (humanChoice === 'scissors' && computerChoice === 'paper')
+  ) {
+    return humanScore++;
+  } else if (humanChoice === computerChoice) {
+    return;
+  } else {
+    return computerScore++;
+  }
+}
+
+function playGame() {
+  for (let i = 1; i < 6; i++) {
+    playRound(getHumanChoice(), getComputerChoice());
+  }
+
+  if (humanScore > computerScore) {
+    console.log(
+      `You win! Your score: ${humanScore}, computer score: ${computerScore}`
+    );
+  } else if (humanScore < computerScore) {
+    console.log(
+      `Computer wins! Your score: ${humanScore}, computer score: ${computerScore}`
+    );
+  } else if (humanScore === computerScore) {
+    console.log(
+      `It\'s a tie! Your score: ${humanScore}, computer score: ${computerScore}`
+    );
+  }
+}
+
+playGame();
